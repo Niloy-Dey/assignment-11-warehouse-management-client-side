d7be61719170283dc6393cef0d42c0cbe94efd84
Niloy-Dey <niloydeyce@gmail.com>
2022-05-08 00:58:49 +0600

    login and sign up design done

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,10 +1,38 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './Login.css'
 const Login = () => {
+    const signInWithEmail = (event) =>{
+        const email= event.target.value;
+        const password = event.target.value; 
+    }
+
+    const handleGoogleLogin = () =>{
+
+    }
     return (
-        <div>
-            
-        </div>
+        <div className=' w-100 mt-5 pt-5'>
+
+           <form 
+            onSubmit={signInWithEmail}
+        >
+            <h1 className="login">LOGIN</h1>
+            <div className="form-container mx-auto">
+                <div className="container login-container">
+                    <label htmlFor="mail"><strong>E-mail</strong></label>
+                    <input className="input-style"  type="text" placeholder="Enter E-mail" name="mail" required />
+                    <label htmlFor="psw"><strong>Password</strong></label>
+                    <input className="input-style" type="password" placeholder="Enter Password" name="psw" required />
+                </div>
+                <br />
+                <p>New to refrigerator world?  <Link to="/signup">Sign up</Link></p>
+                <br />
+                <button type="submit" className="submit w-50"><strong>LOGIN</strong></button>
+                <br />
+                <button onClick={handleGoogleLogin} className="btn btn-warning w-50 py-3 fw-bold">Continue With Google</button>
+            </div>
+        </form >
+           </div>
     );
 };
 
--- src/Component/SignUp/SignUp.js
+++ src/Component/SignUp/SignUp.js
@@ -1,9 +1,37 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './SignUp.css'
 const SignUp = () => {
+    const singUp = (event) =>{
+
+    }
+
+    const handleGoogleLogin= ()=>{
+
+    }
     return (
         <div>
-            
+            <form onSubmit={singUp}>
+            <h1 className="signup mt-5">SIGN UP</h1>
+            {/* <p className="text-danger text-center">{error}</p> */}
+            <div className="form-container mx-auto">
+                <div className="container signup-container">
+                    <label htmlFor="uname"><strong>Username</strong></label>
+                    <input className="input-style" type="text" placeholder="Enter Username" name="uname" required />
+                    <label htmlFor="mail"><strong>E-mail</strong></label>
+                    <input className="input-style"  type="text" placeholder="Enter E-mail" name="mail" required />
+                    <label htmlFor="psw"><strong>Password</strong></label>
+                    <input className="input-style" type="password" placeholder="Enter Password" name="psw" required />
+                </div>
+                <br />
+                <p>Already have an account?  <Link  to="/login">Log in</Link></p>
+                <br />
+                <button type="submit" className="submit w-50 "><strong>SIGN UP</strong></button>
+                <br />
+
+                <button onClick={handleGoogleLogin} className="btn btn-warning w-50 py-3 fw-bold">Continue With Google</button>
+            </div>
+        </form>
         </div>
     );
 };
