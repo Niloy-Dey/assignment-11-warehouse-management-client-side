c45fc71f6233d4716f60b96b03940f98903aaea4
Niloy-Dey <niloydeyce@gmail.com>
2022-05-08 02:03:39 +0600

    footer work done

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            
+                <div className='bg-dark w-100 '>
+                <h1 className='text-white text-center mt-5 py-5 '>copy right &copy; 2022</h1>
+                <h5 className='text-white text-center py-3 '> All right Reserved Refrigerator world </h5>
+        </div>
         </div>
     );
 };
